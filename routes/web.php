cb18d8288988d186b985bcf24ea016ae64e213fb
summer <13041203572@163.com>
2019-12-11 18:26:44 +0800

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//主页
+Route::get("/","StaticPagesController@home");
+
+//帮助页
+Route::get("/help","StaticPagesController@help");
+
+//关于页
+Route::get("/about","StaticPagesController@about");
+
+
