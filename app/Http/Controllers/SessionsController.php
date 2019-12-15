fac0dee5280113b4dd414b2a3451e938fc5ecc21
summer <13041203572@163.com>
2019-12-15 19:26:00 +0800

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+class SessionsController extends Controller
+{
+    //新建登陆视图
+    public function create(){
+        return view('sessions.create');
+    }
+
+    //验证视图信息
+    public function store(Request $request){
+        $credentials=$this->validate($request,[
+            'email'=>'required|email|max:255',
+            'password'=>'required'
+        ]);
+        if(Auth::attempt($credentials)){
+            //登陆成功后
+            session()->flash('success','欢迎回来！');
+            return redirect()->route('users.show',[Auth::user()]);
+        }else{
+            //登陆失败后
+            session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
+            return redirect()->back()->withInput();
+        }
+        return;
+    }
+}
