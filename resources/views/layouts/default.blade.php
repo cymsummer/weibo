3b4f4ac46afa156c58c93b32bd9fec03a6bb72e3
summer <13041203572@163.com>
2019-12-15 19:42:59 +0800

    登陆成功后导航逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,13 +6,17 @@
   <link rel="stylesheet" href="{{mix('/css/app.css')}}">
 </head>
 <body>
+
   @include('layouts._header')
+
   <div class="container">
-  <div class="offset-md-1 col-md-10">
-    @include('shared._message')
-    @yield('content')
-    @include('layouts._footer')
+    <div class="offset-md-1 col-md-10">
+      @include('shared._message')
+      @yield('content')
+      @include('layouts._footer')
+    </div>
   </div>
-</div>
+
+  <script src="{{mix('js/app.js')}}"></script>
 </body>
 </html>
