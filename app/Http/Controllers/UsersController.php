dff9842ed8efa4f91ed565269a540c8139b35cdb
summer <13041203572@163.com>
2019-12-15 18:10:01 +0800

    增加CSRF验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
@@ -16,5 +17,15 @@ class UsersController extends Controller
     //列表展示
     public function show(User $user){
         return view('users.show',compact('user'));
+    }
+
+    //数据信息验证
+    public function store(Request $request){
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'email'=>'required|email|unique:users|max:255',
+            'password'=>'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
