c007a7346c29c2279f6a8495dafeed6ac6047af5
summer <13041203572@163.com>
2019-12-15 20:17:10 +0800

    更新用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,7 +12,6 @@ class UsersController extends Controller
     public function create()
     {
         return view("users.create");
-
     }
 
     //列表展示
@@ -27,17 +26,34 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-
         $user=User::create([
             'name'=>$request->name,
             'email'=>$request->email,
             'password'=>bcrypt($request->password),
         ]);
-
         Auth::login($user);
-
         session()->flash('success','欢迎，您将在这里开启一段新的旅程～');
-
         return redirect()->route('users.show',[$user]);
+    }
+
+    //编辑页面
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+
+    //修改用户信息
+    public function update(User $user,Request $request){
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'password'=>'nullable|confirmed|min:6'
+        ]);
+        $data=[];
+        $data['name']=$request->name;
+        if($request->password){
+            $data['password']=bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash("success","个人资料更新成功！");
+        return redirect()->route('users.show',$user);
     }
 }
