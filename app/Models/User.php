97a14e926aa50463e2cf1b2f91632a06a1893b25
chuyumi <13041203572@163.com>
2020-10-29 14:35:02 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
         $hash=md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
+    //一个用户拥有多个微博
+    public function statuses(){
+        return $this->hasMany(Status::class);
+    }
 }
