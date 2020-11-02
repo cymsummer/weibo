ac9c1e38dd5ae344203d07fd009792330f4cf5cf
chuyumi <13041203572@163.com>
2020-11-02 16:38:30 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,12 @@ class User extends Authenticatable
     }
 
     public function feed(){
-        return $this->statuses()->orderBy('created_at','desc');
+        $user_ids=$this->followings->pluck('id')->toArray();
+        array_push($user_ids,$this->id);
+
+        return Statuses::whereIn('user_id',$user_ids)
+            ->with('user')
+            ->orderBy('created_at','desc');
     }
 
     public function followers(){
@@ -82,4 +87,5 @@ class User extends Authenticatable
     public function isFollowing($user_id){
         return $this->followings->contains($user_id);
     }
+
 }
