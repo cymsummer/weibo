9c695b4bfe5c34d50634f6e72a940ac5fd99d18b
chuyumi <13041203572@163.com>
2020-11-02 17:24:22 +0800

    关注用户动态流

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,7 +4,6 @@
 @section('content')
   <div class="row">
     <div class="offset-md-2 col-md-8">
-<<<<<<< HEAD
       <div class="col-md-12">
         <div class="offset-md-2 col-md-8">
           <section class="user_info">
@@ -28,29 +27,6 @@
 
 
         </div>
-=======
-      <div class="offset-md-2 col-md-8">
-
-        <section class="user_info">
-          @include('shared._user_info', ['user' => $user])
-        </section>
-
-        <section class="status">
-          @if($statuses->count()>0)
-            <ul class="list-unstyled">
-              @foreach($statuses as $status)
-                @include('statuses._status')
-              @endforeach
-            </ul>
-            <div class="mt-5">
-              {!! $statuses->render() !!}
-            </div>
-          @else
-            <p>没有数据！</p>
-          @endif
-        </section>
-
->>>>>>> user-statuses
       </div>
     </div>
   </div>
